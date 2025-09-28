ae4b4e7326473455942c50b251522e25e5548e10
arielpincayy <arielpincay812@gmail.com>
2025-09-28 15:50:12 -0500

    Refactor relax_population calls in deap_sga_protein class
    - Removed the unused 'True' argument from relax_population method calls in both mutation processes.
    - Updated the protein01_relaxed.pdb.txt file to reflect changes in random seed initialization and loading times for the Dunbrack library.

--- prot_GA.py
+++ prot_GA.py
@@ -310,7 +310,7 @@ class deap_sga_protein:
             offspring_list = self.my_protein_problem.mutate_population(argument)
 
             #Fast relax
-            offspring_output_pdbfiles = self.my_protein_problem.relax_population(offspring_output_pdbfiles, True)
+            offspring_output_pdbfiles = self.my_protein_problem.relax_population(offspring_output_pdbfiles)
     
             #Evaluate in parallel
             fitness = self.my_protein_problem.fitnessPop(offspring_output_pdbfiles)
@@ -435,7 +435,7 @@ class deap_sga_protein:
             offspring = self.my_protein_problem.mutate_population(argument)
 
             #Fast relax
-            offspring_output_pdbfiles = self.my_protein_problem.relax_population(offspring_output_pdbfiles, True)
+            offspring_output_pdbfiles = self.my_protein_problem.relax_population(offspring_output_pdbfiles)
             population_output_pdbfiles = list(map(lambda file: file.replace(".pdb", "_relaxed.pdb") if not file.endswith("_relaxed.pdb") else file, population_output_pdbfiles))
 
             #Evaluate in parallel
